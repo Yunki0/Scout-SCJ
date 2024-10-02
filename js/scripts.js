007079b1b20bbbf64c4fc6fd9e237f4ae1c5dfcb
Yunki0 <desirelionel109@gmail.com>
2024-10-02 14:29:34 +0000

    reorganisation

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,4 @@
-// window.addEventListener('load', function() {
-//     const loader = document.getElementById('loader');
-//     const content = document.getElementById('content');
-
-//     // Délai de 3 secondes (3000 millisecondes) avant d'afficher le contenu
-//     setTimeout(function() {
-//         loader.style.display = 'none'; // Masque le loader
-//         content.classList.remove('hidden'); // Affiche le contenu
-//     }, 3000); // Ajustez le délai ici
-// });
-
- window.onload = function() {
+window.onload = function() {
      const loader = document.getElementById('loader');
 //     loader.style.display = 'none';
     
