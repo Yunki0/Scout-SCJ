f6db40dcac6b6bb04e34bbc88392c1412ca92942
Yunki0 <desirelionel109@gmail.com>
2024-10-21 15:06:02 +0000

    NIL

--- js/zoom.js
+++ js/zoom.js
@@ -1,25 +1,3 @@
-// // Récupère tous les éléments image avec la classe 'zoomable'
-// const images = document.querySelectorAll('.zoomable');
-// const zoomOverlay = document.getElementById('zoomOverlay');
-// const zoomedImage = document.getElementById('zoomedImage');
-// const closeZoom = document.getElementById('closeZoom');
-// const body = document.body; // Récupère l'élément body
-
-// // Ajoute un événement de clic à chaque image
-// images.forEach(image => {
-//   image.addEventListener('click', () => {
-//     zoomedImage.src = image.src; // Met à jour la source de l'image zoomée
-//     zoomOverlay.style.display = 'flex'; // Affiche le conteneur de zoom
-//     body.classList.add('no-scroll'); // Désactive le défilement
-//   });
-// });
-
-// // Fermer l'image zoomée lorsque l'utilisateur clique sur 'X'
-// closeZoom.addEventListener('click', () => {
-//   zoomOverlay.style.display = 'none'; // Cache le conteneur de zoom
-//   body.classList.remove('no-scroll'); // Réactive le défilement
-// });
-
 // Récupère tous les éléments image avec la classe 'zoomable'
 const images = document.querySelectorAll('.zoomable');
 const zoomOverlay = document.getElementById('zoomOverlay');
